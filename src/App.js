4dc09a0fa7948fa19a3a188c8d86503f47ba5b97
Riyad0821 <mdriyad0821@gmail.com>
2022-10-01 15:06:15 +0600

    Add styling to product card to show image and text separately

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import products from "./data/products.json"
 
 function App() {
   return (
-    <div>
-      <div>
+    <div className="bg-gray-200">
+      <div className='md:w-1/2 mx-auto'>
         {products.map((product) => (
           <Product product={product} key={product.id} />
         ))}
